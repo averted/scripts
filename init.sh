dd8119314e56230efa3d3d190597a8d9699d0407
averted <vitiuk.alex@gmail.com>
2016-08-24 11:37:11 -0400

    Updating init script

--- init.sh
+++ init.sh
@@ -1,2 +1,30 @@
+#!/bin/bash
+# OSX initialization script
+
+# keyboard settings
 # defaults write -g InitialKeyRepeat -int 11 # (normal - 15)
 # defaults write -g KeyRepeat -int 2 # (normal - 2)
+
+# fetch git dotfiles repo
+mkdir -p ~/git
+cd ~/git
+git clone git@github.com:averted/dotfiles.git
+
+# sync .vim
+cp -Rn ~/git/dotfiles/.vim ~
+
+# sync .vimrc
+cp -n ~/git/dotfiles/.vimrc ~
+
+# sync .bash_profile
+cp -n ~/git/dotfiles/.bash_profile ~
+
+# sync .bashrc
+cp -n ~/git/dotfiles/.bashrc ~
+
+# sync .gitconfig
+if cp -n ~/git/dotfiles/.gitconfig ~; then
+  echo - Dont forget to set email in .gitconfig!
+fi
+
+exit 0
