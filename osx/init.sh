75562e0c87d5912e171a1b750beccd2a3b196cb6
averted <vitiuk.alex@gmail.com>
2017-08-25 13:18:19 -0400

    Updating osx initialization script

--- osx/init.sh
+++ osx/init.sh
@@ -7,21 +7,32 @@
 # fetch git dotfiles repo
 mkdir -p ~/git
 cd ~/git
+
 git clone git@github.com:averted/dotfiles.git
+git clone git@github.com:averted/walls.git
 
 # sync .vim
+echo "Syncing: .vim"
 cp -Rn ~/git/dotfiles/.vim ~
 
 # sync .vimrc
+echo "Syncing: .vimrc"
 cp -n ~/git/dotfiles/.vimrc ~
 
 # sync .bash_profile
+echo "Syncing: .bash_profile"
 cp -n ~/git/dotfiles/.bash_profile ~
 
 # sync .bashrc
+echo "Syncing: .bashrc"
 cp -n ~/git/dotfiles/.bashrc ~
 
+# sync walls
+echo "Syncing: Desktop Pictures"
+sudo cp -n ~/git/walls/* /Library/Desktop\ Pictures/
+
 # sync .gitconfig
+echo "Syncing: .gitconfig"
 if cp -n ~/git/dotfiles/.gitconfig ~; then
   echo - Dont forget to set email in .gitconfig!
 fi
