e6d0aa706e398424308e863100463ceed6ea4c34
averted <vitiuk.alex@gmail.com>
2020-02-18 12:42:55 -0500

    Bump keyrepeat delay to 1

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,5 +1,5 @@
 defaults write NSGlobalDomain InitialKeyRepeat -int 10 # 12 ninja
-defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain KeyRepeat -int 1
 
 # disable headphones buttons opening iTunes
 # launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
